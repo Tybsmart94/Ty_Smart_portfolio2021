73ee1caa05ab0282d94b47af601edcba7d40cdd4
Ty Smart <ty.smart@tooeleschools.org>
2021-05-13 12:56:06 -0600

    Doodle Jump game

---
+++ jumpy_platformer/settings.py
@@ -0,0 +1,41 @@
+# game options/settings
+TITLE = "Jump Boi"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+FONT_NAME = 'arial'
+HS_FILE = "highscore.txt"
+SPRITESHEET = "spritesheet_jumper.png"
+
+#Player priperties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.95
+PLAYER_JUMP = -45
+
+#Game properties
+BOOST_POWER = 150
+POW_SPAWN_PCT = 7
+MOB_FREQ = 5000
+PLAYER_LAYER = 2
+PLATFORM_LAYER = 1
+POW_LAYER = 1
+MOB_LAYER = 2
+CLOUD_LAYER = 0
+
+#Starting Platforms
+PLATFORM_LIST = [(0, HEIGHT - 60 ),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4 - 50),
+                 (125, HEIGHT - 350),
+                 (350, 200),
+                 (175, 100)]
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+LIGHTBLUE = (0, 155, 155)
+BGCOLOR = LIGHTBLUE
